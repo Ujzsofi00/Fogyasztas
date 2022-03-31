37620eed75e855f35323e27522ee4d00cb25c0cc
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2022-03-31 14:24:42 +0200

    Kesz

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let F = document.getElementById('fogyasztas').value;
-let V = document.getElementById('tank').value;
-let Ut = document.getElementById('ut').value;
+function szamol() {
+    let F = document.getElementById('fogyasztas').value;
+    let V = document.getElementById('tank').value;
+    let Ut = document.getElementById('ut').value;
 
+    let megtehetoUt = V * 100 / F;
+
+    if (megtehetoUt < Ut) {
+        document.getElementById('megoldas').innerHTML = "Az út során tankolni kell!"
+    } else {
+        document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül"
+    }
+}
